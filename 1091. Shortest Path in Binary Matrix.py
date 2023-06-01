ffa711aa5d07b27baf3c10556850d156b036e6ce
pranavagni1516 <pranav.agni1516@gmail.com>
2023-06-01 17:27:51 +0530

    Create 1091. Shortest Path in Binary Matrix.py

---
+++ 1091. Shortest Path in Binary Matrix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n=len(grid)
+        if(grid[0][0] or grid[n-1][n-1]):return -1
+
+        q=[(0,0,1)]
+        grid[0][0]=1
+
+        for i,j,d in q:
+            if (i==n-1 and j==n-1):return d
+            direction=[(i-1,j-1),(i-1,j),(i-1,j+1),(i,j-1),(i,j+1),(i+1,j-1),(i+1,j),(i+1,j+1)]
+            for x,y in direction:
+                if 0<=x<n and 0<=y<n and grid[x][y]==0:
+                    grid[x][y]=1
+                    q.append((x,y,d+1))
+        
+        return -1
