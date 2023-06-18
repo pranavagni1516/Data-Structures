437294de7e370b500fb7e28ea8615b6542e24c19
pranavagni1516 <pranav.agni1516@gmail.com>
2023-06-18 15:18:36 +0530

    Create Ticket Counter.cpp

---
+++ Ticket Counter.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int distributeTicket(int N, int K) {
+        // code here
+        int ans=0,i=1,j=N;
+        bool a=false;
+        while(i<=j){
+            if(!a){
+                i+=K;
+                ans=j;
+            }
+            else {
+                j-=K;ans=i;
+            }
+            a=!a;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        int K;
+        scanf("%d",&K);
+        
+        Solution obj;
+        int res = obj.distributeTicket(N, K);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
